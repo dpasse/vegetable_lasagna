96a8402e57c3455190b771a440850f37e0deecbe
dpasse <dpasse@mpirik.com>
2019-11-07 07:34:35 -0600

    forgot the rest of it

--- projects/regex_gp/package/transformer/StringToMapping.py
+++ projects/regex_gp/package/transformer/StringToMapping.py
@@ -1,12 +1,12 @@
-class BinaryToMapping:
+class StringToMapping:
     
     mapping = {}
     
     def __init__(self, mapping):
         self.mapping = mapping
         
-    def transform(self, binary_string):
-        return self.mapping[binary_string]
+    def transform(self, key):
+        return self.mapping[key]
 
-    def transform_array(self, binary_array):
-        return [ self.transform(binary_string) for binary_string in binary_array ]
+    def transform_array(self, array_of_keys):
+        return [ self.transform(key) for key in array_of_keys ]
