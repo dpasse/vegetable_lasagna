a7a31a258d019195fdc100538a1603a5cc56f82d
dpasse <dpasse@mpirik.com>
2019-11-08 17:11:14 -0600

    dynamic individual poc, refactoring, combining common code

--- projects/regex_gp/package/ga/BinaryGeneFactory.py
+++ projects/regex_gp/package/ga/BinaryGeneFactory.py
@@ -1,17 +1,21 @@
 import numpy as np
-from ..transformer import IntegerToBinaryString
+from ..transformers import IntegerToBinaryString
 
 
 class BinaryGeneFactory():
     
     transformer = None
+    binary_start = -1
+    binary_end = -1
     
-    def __init__(self, max_length):
-        self.transformer = IntegerToBinaryString.IntegerToBinaryString(max_length)
+    def __init__(self, binary_start, binary_end, max_binary_length):
+        self.binary_start = binary_start
+        self.binary_end = binary_end
+        self.transformer = IntegerToBinaryString.IntegerToBinaryString(max_binary_length)
     
-    def create(self, start, end):
-        random_integer = np.random.randint(start, end)
+    def create(self):
+        random_integer = np.random.randint(self.binary_start, self.binary_end)
         return self.transformer.transform(random_integer)
     
-    def create_many(self, start, end, total):
-        return [ self.create(start, end) for _ in range(total) ]
+    def create_many(self, total):
+        return [ self.create() for _ in range(total) ]
