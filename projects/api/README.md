d7a6afcc6322f5aabe23bc7500efeef6ae0438f9
dpasse <dpasse@mpirik.com>
2019-11-09 05:52:05 -0600

    formatting, setup flask api

---
+++ projects/api/README.md
@@ -0,0 +1,4 @@
+#### Run:
+
+ 1. export FLASK_APP=projects/api/app.py
+ 2. flask run
--- projects/regex_gp/package/ga/AbstractFitness.py
+++ projects/regex_gp/package/ga/AbstractFitness.py
@@ -2,10 +2,10 @@ from abc import ABC, abstractmethod
 
 
 class AbstractFitness(ABC):
-    
-    def __init__():
+
+    def __init__(self):
         pass
-    
+
     @abstractmethod
     def evaluate(self, individual, iterations = 1000, display_logging = False):
         pass
