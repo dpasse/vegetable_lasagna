60831b897065f0d7ee2d0a6915896203a5ac3017
dpasse <dpasse@mpirik.com>
2019-11-10 17:30:29 -0600

    started setup file for previous poc

---
+++ projects/regex_gp/package/ga/setups/DynamicWithStaticEnding.py
@@ -0,0 +1,111 @@
+import re
+import numpy as np
+from ..AbstractFitness import AbstractFitness
+
+
+class Fitness(AbstractFitness):
+    to_regex = None
+    expected_match = None
+    static_ending = None
+    
+    def __init__(self, to_regex, static_ending, expected_match, text):
+        self.to_regex = to_regex
+        self.static_ending = static_ending
+        self.expected_match = expected_match
+        self.text = text
+        
+        super()
+
+    previous_length = -1
+    previous_fitness = -1
+    
+    def evaluate_genes(self, individual, display_logging = False):
+        regex = ''
+        fitness = 0
+        length_of_individual = len(individual)
+        reverse = np.flip(self.to_regex.transform_to_array(individual))
+        for i, regex_item in enumerate(reverse):
+            temp_regex = regex_item + regex
+            
+            ## encourage individual regex correctness,
+            pattern = re.compile(temp_regex + self.static_ending, re.IGNORECASE)
+            matches = pattern.findall(self.text)
+            if len(matches) > 0:
+                fitness += (( 1 - (i / length_of_individual) ) / length_of_individual)
+            else:
+                ## when item is wrong,
+                temp_regex = '(.|\s)' + regex
+            
+            regex = temp_regex
+            
+        perfect_score = np.array([ 
+            (( 1 - (i / length_of_individual) ) / length_of_individual)
+            for i 
+            in range(length_of_individual) ]
+        ).sum()
+        
+        return fitness / perfect_score
+    
+    def evaluate_individual(self, individual, display_logging = False):
+        regex = self.to_regex.transform(individual) + self.static_ending
+        pattern = re.compile(regex, re.IGNORECASE)
+        
+        fitness = 0
+        
+        # encourage matches, but less is better.
+        matches = pattern.findall(self.text)
+        if len(matches) == 1:
+            fitness += ( 1 / len(matches) )
+        
+        # encourage the only match being the correct match.
+        match = pattern.search(self.text)
+        if match is not None and float(match.group(1)) == self.expected_match:
+            fitness += 1
+            
+        return fitness
+        
+        
+    def evaluate(self, individual, display_logging = False):
+        new_fitness = 0.0
+        
+        new_fitness += self.evaluate_genes(individual, display_logging)
+        new_fitness += self.evaluate_individual(individual, display_logging)
+        
+        new_length = len(individual)
+        previous_fitness = self.previous_fitness
+        self.previous_fitness = new_fitness
+        
+        if previous_fitness == new_fitness:
+            if self.previous_length <= new_length:
+                new_fitness += 1 # encourage growth over shrinking,
+        
+        self.previous_length = new_length
+        
+        return new_fitness / 4
+
+class Mutator():
+    
+    gene_factory = None
+    
+    def __init__(self, gene_factory):
+        self.gene_factory = gene_factory
+
+    def gene_mutator(self, gene, display_logging = False):
+        precentage = np.random.rand()
+        if precentage < .08:
+            new_gene = self.gene_factory.create()
+            gene = new_gene
+
+        return gene
+
+    def individual_height_mutator(self, individual, display_logging = False):
+        precentage = np.random.rand()
+        if precentage < .10:
+            gene = self.gene_factory.create()
+            individual = [gene] + individual # grow to the left,
+
+        length = len(individual)
+        if precentage > .90 and length > 0:
+            individual = individual[1:] # remove from the left,
+
+        return individual
